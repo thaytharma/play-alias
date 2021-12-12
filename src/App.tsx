d1d0375363a2db1f2beb48acdf8c9ad4398f2fa5
Thayanan Tharmapalan <thayanan@gmail.com>
2021-12-12 22:37:28 +0100

    fix: remove word change when changing lang

--- src/App.tsx
+++ src/App.tsx
@@ -41,7 +41,6 @@ function App() {
 
   const handleChangeLanguage = () => {
     setLanguage(isEnglish ? "NO" : "EN");
-    handleWordChange();
     /*     setLanguageInUrl(language);
      */
   };
