38df7785a119d5be0f957f6fee0a37fbcfc65d98
Thayanan Tharmapalan <thayanan.tharmapalan@dnb.no>
2026-06-14 22:50:12 +0200

    chore: remove unused excludedWords service

--- src/services/words.js
+++
@@ -1,6 +0,0 @@
-
-export default function excludedWords() {
-   const words = ["what", "is", "slave", "actually", "the", "of", "universe", "all", "that", "is", "to", "it","hay"];
-
-   return words;
-}
